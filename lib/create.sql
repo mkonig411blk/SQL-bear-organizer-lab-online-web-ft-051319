a4415b1b409052b6eecb16a73de6eccf97ce17e4
Margot Konig <margot.konig@blackrock.com>
2019-06-06 23:54:06 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,2 +1,1 @@
-CREATE TABLE bears ("id","name","age","gender","color","temperament","alive"), (INTEGER, TEXT, INTEGER,TEXT,TEXT,TEXT,INTEGER)
-);
+CREATE TABLE bears (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender TEXT, color TEXT,temperament TEXT, alive INTEGER);
