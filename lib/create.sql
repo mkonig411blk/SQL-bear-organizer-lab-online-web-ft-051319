91ef18557b01da5a2cfa0a524f4f9ae5c45af4ee
Margot Konig <margot.konig@blackrock.com>
2019-06-06 23:56:22 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,3 +1,10 @@
 CREATE TABLE bears (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender TEXT, color TEXT,temperament TEXT, alive INTEGER);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate',27,female,brown,angry,1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy'',27,male,brown,angry,1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha'',27,male,brown,angry,1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown'',27,male,brown,angry,1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa'',27,male,brown,angry,1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch'',27,male,brown,angry,1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy'',27,male,brown,angry,1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy'',27,male,brown,angry,1);
