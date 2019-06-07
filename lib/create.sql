625cf915f64bde5a5b3f9a77bdf9fe6c9f4f7d60
Margot Konig <margot.konig@blackrock.com>
2019-06-07 00:03:08 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -10,8 +10,8 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy'
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL,27,'female','brown','angry',1);
 
 select name, age from bears where gender = 'female';
-"select name from bears order by name asc;"
-"select name, age from bears where alive = 1 order by age;"
-"select name, age from bears order by age DESC LIMIT 1;"
+select name from bears order by name asc;
+select name, age from bears where alive = 1 order by age;
+select name, age from bears order by age DESC LIMIT 1;
 
 
