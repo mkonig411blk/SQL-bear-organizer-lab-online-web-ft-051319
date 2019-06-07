c1d343b9b46dc5458a45855534e79e0c55f1c980
Margot Konig <margot.konig@blackrock.com>
2019-06-07 00:05:25 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate',27,'female','brown','angry',1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy',27,'female','brown','angry',1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha',27,'female','brown','angry',1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown',27,'female','brown','angry',1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa',27,'female','brown','angry',1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch',27,'female','brown','angry',1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy',27,'female','brown','angry',1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL,27,'female','brown','angry',1);
