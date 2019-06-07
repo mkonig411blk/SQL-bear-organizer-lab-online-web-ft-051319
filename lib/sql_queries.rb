71c7211572018cbd8694e9904ad6ec28085bcee4
Margot Konig <margot.konig@blackrock.com>
2019-06-07 00:07:53 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,15 +15,15 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "select name, age from bears order by age ASC LIMIT 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "select color, count(color) form bears group by color order by count(color) LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "select count(temperament) from bears where temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
